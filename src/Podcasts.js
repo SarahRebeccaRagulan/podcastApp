509c930be1dcf2ce597dcb24700116e1469602c8
ragulan <ragulanravi21@gmail.com>
2021-04-14 14:52:01 -0400

    added podcast component.

---
+++ src/Podcasts.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const Podcasts = () => {
+  return (
+    <div>
+      <h1>Podcasts that match your travel time:</h1>
+
+      <div className='podcastCard'>
+        <img src='http://placecorgi.com/200/200' />
+        <p>Title:</p>
+        <p>Description:</p>
+        <p>Episode Name</p>
+        <p>Length:</p>
+        <p>Explicit</p>
+      </div>
+
+      <div className='podcastCard'>
+        <img src='http://placecorgi.com/200/200' />
+        <p>Title:</p>
+        <p>Episode Name</p>
+        <p>Description:</p>
+        <p>Length:</p>
+        <p>Explicit</p>
+      </div>
+
+      <div className='podcastCard'>
+        <img src='http://placecorgi.com/200/200' />
+        <p>Title:</p>
+        <p>Description:</p>
+        <p>Episode Name</p>
+        <p>Length:</p>
+        <p>Explicit</p>
+      </div>
+
+      <div className='podcastCard'>
+        <img src='http://placecorgi.com/200/200' />
+        <p>Title:</p>
+        <p>Description:</p>
+        <p>Episode Name</p>
+        <p>Length:</p>
+        <p>Explicit</p>
+      </div>
+
+      <div className='podcastCard'>
+        <img src='http://placecorgi.com/200/200' />
+        <p>Title:</p>
+        <p>Description:</p>
+        <p>Episode Name</p>
+        <p>Length:</p>
+        <p>Explicit</p>
+      </div>
+
+      <div className='podcastCard'>
+        <img src='http://placecorgi.com/200/200' />
+        <p>Title:</p>
+        <p>Description:</p>
+        <p>Episode Name</p>
+        <p>Length:</p>
+        <p>Explicit</p>
+      </div>
+    </div>
+  )
+}
+
+export default Podcasts

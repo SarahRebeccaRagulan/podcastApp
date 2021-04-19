03875367c8ccf8eb186e844729787bbf6583bc79
sarahpilato <sarahkitap@gmail.com>
2021-04-19 13:45:46 -0400

    flexed podcast cards

--- src/Podcasts.js
+++ src/Podcasts.js
@@ -2,61 +2,62 @@ import React from 'react'
 
 const Podcasts = () => {
   return (
-    <div>
+    <div className="wrapper">
       <h1>Podcasts that match your travel time:</h1>
+      <div className="podcastParent"> 
+        <div className='podcastCard'>
+          <img src='http://placecorgi.com/200/200' />
+          <p>Title:</p>
+          <p>Description:</p>
+          <p>Episode Name</p>
+          <p>Length:</p>
+          <p>Explicit</p>
+        </div>
 
-      <div className='podcastCard'>
-        <img src='http://placecorgi.com/200/200' />
-        <p>Title:</p>
-        <p>Description:</p>
-        <p>Episode Name</p>
-        <p>Length:</p>
-        <p>Explicit</p>
-      </div>
+        <div className='podcastCard'>
+          <img src='http://placecorgi.com/200/200' />
+          <p>Title:</p>
+          <p>Episode Name</p>
+          <p>Description:</p>
+          <p>Length:</p>
+          <p>Explicit</p>
+        </div>
 
-      <div className='podcastCard'>
-        <img src='http://placecorgi.com/200/200' />
-        <p>Title:</p>
-        <p>Episode Name</p>
-        <p>Description:</p>
-        <p>Length:</p>
-        <p>Explicit</p>
-      </div>
+        <div className='podcastCard'>
+          <img src='http://placecorgi.com/200/200' />
+          <p>Title:</p>
+          <p>Description:</p>
+          <p>Episode Name</p>
+          <p>Length:</p>
+          <p>Explicit</p>
+        </div>
 
-      <div className='podcastCard'>
-        <img src='http://placecorgi.com/200/200' />
-        <p>Title:</p>
-        <p>Description:</p>
-        <p>Episode Name</p>
-        <p>Length:</p>
-        <p>Explicit</p>
-      </div>
+        <div className='podcastCard'>
+          <img src='http://placecorgi.com/200/200' />
+          <p>Title:</p>
+          <p>Description:</p>
+          <p>Episode Name</p>
+          <p>Length:</p>
+          <p>Explicit</p>
+        </div>
 
-      <div className='podcastCard'>
-        <img src='http://placecorgi.com/200/200' />
-        <p>Title:</p>
-        <p>Description:</p>
-        <p>Episode Name</p>
-        <p>Length:</p>
-        <p>Explicit</p>
-      </div>
+        <div className='podcastCard'>
+          <img src='http://placecorgi.com/200/200' />
+          <p>Title:</p>
+          <p>Description:</p>
+          <p>Episode Name</p>
+          <p>Length:</p>
+          <p>Explicit</p>
+        </div>
 
-      <div className='podcastCard'>
-        <img src='http://placecorgi.com/200/200' />
-        <p>Title:</p>
-        <p>Description:</p>
-        <p>Episode Name</p>
-        <p>Length:</p>
-        <p>Explicit</p>
-      </div>
-
-      <div className='podcastCard'>
-        <img src='http://placecorgi.com/200/200' />
-        <p>Title:</p>
-        <p>Description:</p>
-        <p>Episode Name</p>
-        <p>Length:</p>
-        <p>Explicit</p>
+        <div className='podcastCard'>
+          <img src='http://placecorgi.com/200/200' />
+          <p>Title:</p>
+          <p>Description:</p>
+          <p>Episode Name</p>
+          <p>Length:</p>
+          <p>Explicit</p>
+        </div>
       </div>
     </div>
   )

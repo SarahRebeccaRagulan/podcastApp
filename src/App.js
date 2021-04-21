853e1d32de428296f7705f66ddcd6bee3e4a23d7
ragulan <ragulanravi21@gmail.com>
2021-04-21 11:55:33 -0400

    adjusted fonts, fixed form sizing, added bg.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,61 @@
-import "./App.css";
-import "./styles/sass/style.css";
-import Stats from "./Stats";
-import Podcasts from "./Podcasts";
+import './App.css'
+import './styles/sass/style.css'
+import Stats from './Stats'
+import Podcasts from './Podcasts'
 
 function App() {
   return (
-    <div className="App">
-      <header className="wrapper">
+    <div className='App'>
+      <header className='wrapper'>
         <h1>Podcast Prioritizer</h1>
-        <p>Find the right podcast for your next adventure!</p>
-        <p>Don't bike and podcast!</p>
+        <h2>Find the right podcast for your next adventure!</h2>
+        <h3>Don't bike and podcast!</h3>
       </header>
-      <section className="wrapper">
-        <form action="">
-          <div className="inputBox">
-            <label name="startingPoint" aria-label="starting point">
+      <section className='wrapper'>
+        <form action=''>
+          <div className='inputBox'>
+            <label name='startingPoint' aria-label='starting point'>
               <input
-                type="text"
-                placeholder="Starting Point"
-                name="startingPoint"
-                class="nameInput"
+                type='text'
+                placeholder='Starting Point'
+                name='startingPoint'
+                className='nameInput'
               />
             </label>
 
-            <label name="destination" aria-label="destination">
+            <label name='destination' aria-label='destination'>
               <input
-                type="text"
-                placeholder="Destination"
-                name="destination"
-                class="nameInput"
+                type='text'
+                placeholder='Destination'
+                name='destination'
+                className='nameInput'
               />
             </label>
           </div>
-          <button type="submit">Find a Podcast</button>
+          <button type='submit'>Find a Podcast</button>
         </form>
       </section>
       <Stats />
       <Podcasts />
       <footer>
-        <p className="wrapper">
-          Created by{" "}
-          <a href="https://www.ragulancodes.com/" target="_blank">
+        <p className='wrapper'>
+          Created by{' '}
+          <a href='https://www.ragulancodes.com/' target='_blank'>
             Ragulan Ravi
           </a>
-          ,{" "}
-          <a href="https://www.rebeccamacdonald.dev/" target="_blank">
+          ,{' '}
+          <a href='https://www.rebeccamacdonald.dev/' target='_blank'>
             Rebecca MacDonald
-          </a>{" "}
-          and{" "}
-          <a href="https://www.sarahpcodes.com" target="_blank">
+          </a>{' '}
+          and{' '}
+          <a href='https://www.sarahpcodes.com' target='_blank'>
             Sarah Pilato
           </a>
           .
         </p>
       </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Stats.js
+++ src/Stats.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBicycle } from '@fortawesome/free-solid-svg-icons';
-import { faWalking } from '@fortawesome/free-solid-svg-icons';
-
+import { faBicycle } from '@fortawesome/free-solid-svg-icons'
+import { faWalking } from '@fortawesome/free-solid-svg-icons'
 
 const Stats = () => {
   return (
     <div className='wrapper statsContainer'>
       <div>
-        <FontAwesomeIcon className= "icon" icon={ faBicycle } size="3x"/> 
+        <FontAwesomeIcon className='icon' icon={faBicycle} size='3x' />
 
-        <div className='statsBox'><p>Distance:</p>
-        <p>Time:</p></div>
+        <div className='statsBox'>
+          <p>Distance:</p>
+          <p>Time:</p>
+        </div>
       </div>
 
       <div>
-        <FontAwesomeIcon className= "icon" icon={faWalking } size="3x"/>
-        <div class="statsBox"><p>Distance:</p>
-        <p>Time:</p></div>
+        <FontAwesomeIcon className='icon' icon={faWalking} size='3x' />
+        <div className='statsBox'>
+          <p>Distance:</p>
+          <p>Time:</p>
+        </div>
       </div>
     </div>
   )

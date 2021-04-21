0bfc3538ca8442e037add06805450661fa0f4fca
ragulan <ragulanravi21@gmail.com>
2021-04-21 15:06:51 -0400

    added icon borders, fixed positioning on icon.

--- src/Stats.js
+++ src/Stats.js
@@ -1,38 +1,42 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBicycle } from "@fortawesome/free-solid-svg-icons";
-import { faWalking } from "@fortawesome/free-solid-svg-icons";
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBicycle } from '@fortawesome/free-solid-svg-icons'
+import { faWalking } from '@fortawesome/free-solid-svg-icons'
 
 const Stats = () => {
   return (
-    <div className="wrapper statsContainer">
+    <div className='wrapper statsContainer'>
       <div>
-        <span className="iconBorder">
-          <FontAwesomeIcon className="icon" icon={faBicycle} size="3x" />
-        </span>
+        <div className='iconImage'>
+          <div className='iconBorder icon'>
+            <FontAwesomeIcon className='i' icon={faBicycle} size='3x' />
+          </div>
+        </div>
 
-        <div className="statsBox">
+        <div className='statsBox'>
           <p>Distance:</p>
           <p>Time:</p>
         </div>
       </div>
 
       <div>
-        <div className="iconBorder">
-          <FontAwesomeIcon className="icon" icon={faWalking} size="3x" />
+        <div className='iconImage'>
+          <div className='iconBorder icon'>
+            <FontAwesomeIcon className='i' icon={faWalking} size='3x' />
+          </div>
         </div>
-        <div className="statsBox">
+        <div className='statsBox'>
           <p>Distance:</p>
           <p>Time:</p>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 {
   /* <i class=“fas fa-bicycle”></i>
 <i class=“fas fa-walking”></i> */
 }
 
-export default Stats;
+export default Stats

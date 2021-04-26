7a7e385beb09e451bfd1905db6ce55cfbf0aa41d
sarahpilato <sarahkitap@gmail.com>
2021-04-26 12:38:29 -0400

    played around with useState

--- src/App.js
+++ src/App.js
@@ -11,30 +11,6 @@ function App() {
         <h2>Find the right podcast for your next adventure!</h2>
         <h3>Don't bike and podcast!</h3>
       </header>
-      <section className='wrapper'>
-        <form action=''>
-          <div className='inputBox'>
-            <label name='startingPoint' aria-label='starting point'>
-              <input
-                type='text'
-                placeholder='Starting Point'
-                name='startingPoint'
-                className='nameInput'
-              />
-            </label>
-
-            <label name='destination' aria-label='destination'>
-              <input
-                type='text'
-                placeholder='Destination'
-                name='destination'
-                className='nameInput'
-              />
-            </label>
-          </div>
-          <button type='submit'>Find a Podcast</button>
-        </form>
-      </section>
       <Stats />
       <Podcasts />
       <footer>
--- src/Stats.js
+++ src/Stats.js
@@ -1,65 +1,99 @@
 //FUNCTIONAL IMPORTS
-import React from 'react'
-import { useEffect } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 //DESIGN IMPORTS
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBicycle } from '@fortawesome/free-solid-svg-icons'
-import { faWalking } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBicycle } from "@fortawesome/free-solid-svg-icons";
+import { faWalking } from "@fortawesome/free-solid-svg-icons";
 
 // API CALL
 const Stats = () => {
+
+  const [startChoice, setStartChoice] = useState('');
+  const [endChoice, setEndChoice] = useState('');
+  const [results, setResults] = useState('');
+
+  const handleRoute = (event) => {
+    // setResults(event.target.value);
+  }
+
   useEffect(() => {
     axios({
-      url: 'http://www.mapquestapi.com/directions/v2/route?',
-      method: 'GET',
-      dataResponse: 'json',
+      url: "http://www.mapquestapi.com/directions/v2/route?",
+      method: "GET",
+      dataResponse: "json",
       params: {
-        key: 'F0QBceSH4eyAyQtIR0dAcCyKnwirHxxG',
-        routeType: 'fastest',
-        transportMode: 'AUTO',
-        from: 'Toronto',
-        to: 'Calgary',
-        ambiguities: 'ignore',
+        key: "F0QBceSH4eyAyQtIR0dAcCyKnwirHxxG",
+        routeType: "fastest",
+        transportMode: "AUTO",
+        from: `${startPoint}`,
+        to: `${endPoint}`,
+        ambiguities: "ignore",
       },
     }).then((res) => {
-      console.log(res)
-    })
-  }, [])
-
-  // template literals
-  // from: `${startPoint}`,
-  //       to: `${endPoint}`,
+      console.log(res);
+    });
+  }, []);
 
   return (
-    <div className='wrapper statsContainer'>
-      <div>
-        <div className='iconImage'>
-          <div className='iconBorder icon'>
-            <FontAwesomeIcon className='i' icon={faBicycle} size='3x' />
+    <div>
+      <section className="wrapper">
+        <form action="">
+          <div className="inputBox">
+            <label name="startingPoint" aria-label="starting point">
+              <input
+                type="text"
+                placeholder="Starting Point"
+                value={startPoint}
+                name="startingPoint"
+                className="nameInput"
+              />
+            </label>
+
+            <label name="destination" aria-label="destination">
+              <input
+                type="text"
+                placeholder="Destination"
+                value={endPoint}
+                name="destination"
+                className="nameInput"
+              />
+            </label>
+          </div>
+          <button type="submit" onClick={(event) => handleClick(event, )}> Find a Podcast</button>
+        </form>
+      </section>
+
+      <div className="wrapper statsContainer">
+        <div>
+          <div className="iconImage">
+            <div className="iconBorder icon">
+              <FontAwesomeIcon className="i" icon={faBicycle} size="3x" />
+            </div>
+          </div>
+
+          <div className="statsBox">
+            <p>Distance:</p>
+            <p>Time:</p>
           </div>
         </div>
 
-        <div className='statsBox'>
-          <p>Distance:</p>
-          <p>Time:</p>
-        </div>
-      </div>
-
-      <div>
-        <div className='iconImage'>
-          <div className='iconBorder icon'>
-            <FontAwesomeIcon className='i' icon={faWalking} size='3x' />
+        <div>
+          <div className="iconImage">
+            <div className="iconBorder icon">
+              <FontAwesomeIcon className="i" icon={faWalking} size="3x" />
+            </div>
           </div>
-        </div>
-        <div className='statsBox'>
-          <p>Distance:</p>
-          <p>Time:</p>
+          <div className="statsBox">
+            <p>Distance:</p>
+            <p>Time:</p>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stats
+export default Stats;

a0da5ce4d8bce559510ca0999c2552012f3c729e
ragulan <ragulanravi21@gmail.com>
2021-04-26 12:07:05 -0400

    added api params.

--- src/Stats.js
+++ src/Stats.js
@@ -1,31 +1,37 @@
 //FUNCTIONAL IMPORTS
-import React from 'react';
-import { useEffect } from 'react';
-import axios from 'axios';
+import React from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 //DESIGN IMPORTS
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBicycle } from '@fortawesome/free-solid-svg-icons';
-import { faWalking } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBicycle } from '@fortawesome/free-solid-svg-icons'
+import { faWalking } from '@fortawesome/free-solid-svg-icons'
 
-
+// API CALL
 const Stats = () => {
-    useEffect(() => {
-      axios({
-        url: "https://www.mapquestapi.com/directions/v2/route?key=KEY&from=Denver%2C+CO&to=Boulder%2C+CO&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false",
-        method: "GET",
-        dataResponse: "json",
-        params: {
-          client_id: "F0QBceSH4eyAyQtIR0dAcCyKnwirHxxG",
-          routeType: "FASTEST",
-          transportMode: "AUTO"
-          messages: 
-        },
-      }).then((res) => {
-        console.log(res);
-      })
-    }, [])
-  
+  useEffect(() => {
+    axios({
+      url: 'http://www.mapquestapi.com/directions/v2/route?',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        key: 'F0QBceSH4eyAyQtIR0dAcCyKnwirHxxG',
+        routeType: 'fastest',
+        transportMode: 'AUTO',
+        from: 'Toronto',
+        to: 'Calgary',
+        ambiguities: 'ignore',
+      },
+    }).then((res) => {
+      console.log(res)
+    })
+  }, [])
+
+  // template literals
+  // from: `${startPoint}`,
+  //       to: `${endPoint}`,
+
   return (
     <div className='wrapper statsContainer'>
       <div>
@@ -56,4 +62,4 @@ const Stats = () => {
   )
 }
 
-export default Stats;
+export default Stats

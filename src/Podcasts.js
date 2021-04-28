9e186af243e44d994b61022f4d60bb820aaa058f
ragulan <ragulanravi21@gmail.com>
2021-04-28 13:56:14 -0400

    added api setup for podcast comp.

--- src/Podcasts.js
+++ src/Podcasts.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Podcasts = () => {
+  useEffect(() => {
+    axios({
+      url: 'https://listen-api.listennotes.com/api/v2/best_podcasts/',
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        headers: 'd6e3e64e5eec4dd68226157de0098df4',
+      },
+    }).then((res) => {
+      console.log(res)
+      // const distanceData = res.data.route.distance
+      // setResults(distanceData)
+      // const bikeTime = res.data.route.formattedTime
+      // setBicycleTime(bikeTime)
+    })
+  }, [])
+
+  // .header('X-ListenAPI-Key', 'd6e3e64e5eec4dd68226157de0098df4')
+
   return (
     <div className='wrapper'>
       <h2>Podcasts that match your walking time:</h2>
